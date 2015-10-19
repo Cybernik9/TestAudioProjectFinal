5139d82b5db466ba7df9f74807fb38197c77744b
Cybernik9 <yuriihuber@macminissu8.training.local>
2015-10-19 14:28:01 +0300

    Initial Commit

---
+++ TestAudioProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestAudioProject
+//
+//  Created by Yurii Huber on 19.10.15.
+//  Copyright (c) 2015 Yurii Huber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
